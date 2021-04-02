babf276bdf3edf5a5145c94773a921b5d331ebb8
Gustavo <gusta99sant@gmail.com>
2021-04-02 18:40:40 -0300

    chore: Validate user data - POST and PUT.

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { celebrate, Joi, Segments } from 'celebrate';
 import userController from '../controller/UserController';
 
 const usersRouter = Router();
@@ -8,9 +9,31 @@ usersRouter.get('/', userController.read);
 
 usersRouter.get('/:id', userController.detail);
 
-usersRouter.post('/', userController.create);
+usersRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: Joi.object().keys({
+      fullname: Joi.string().min(5).required(),
+      email: Joi.string().email().required(),
+      password: Joi.string().min(3).required(),
+      role: Joi.string().required(),
+    }),
+  }),
+  userController.create,
+);
 
-usersRouter.put('/:id', userController.update);
+usersRouter.put(
+  '/:id',
+  celebrate({
+    [Segments.BODY]: Joi.object().keys({
+      fullname: Joi.string().min(5).required(),
+      password: Joi.string().min(3).required(),
+      role: Joi.string().required(),
+      status: Joi.boolean(),
+    }),
+  }),
+  userController.update,
+);
 
 usersRouter.patch('/:id/:setStatus/', userController.updateStatus);
 
