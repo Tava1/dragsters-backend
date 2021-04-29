fa891e2b7d859fc4283262f8b817e95322f23be1
Gustavo <gusta99sant@gmail.com>
2021-04-29 12:53:52 -0300

    fix: Response, errors and rm unnecessary comments

--- src/controller/DeliveryAddressController.ts
+++ src/controller/DeliveryAddressController.ts
@@ -3,7 +3,6 @@ import { getRepository } from 'typeorm';
 import DeliveryAddress from '../models/DeliveryAddress';
 
 const deliveryAddressController = {
-  // TODO: Create new delivery address
   async create(request: Request, response: Response): Promise<any> {
     const { customerID } = request.params;
 
@@ -35,8 +34,6 @@ const deliveryAddressController = {
         customers_id: customerID,
       });
 
-      console.log(newDeliveryAddress);
-
       const deliveryAddressData = await deliveryAddressRepository.save(
         newDeliveryAddress,
       );
@@ -46,39 +43,42 @@ const deliveryAddressController = {
       return response.status(400).json({ message: 'Ocorreu um erro.' });
     }
   },
-  // TODO: List all delivery address
+
   async read(request: Request, response: Response): Promise<any> {
     const { customerID } = request.params;
+
     try {
       const deliveryAddressRepository = getRepository(DeliveryAddress);
 
       const deliveryAddress = await deliveryAddressRepository.find({
         where: { customers_id: customerID },
       });
-      return response.json(deliveryAddress);
+
+      return response.status(200).json(deliveryAddress);
     } catch (error) {
       return response.status(400).json({ message: 'Ocorreu um erro.' });
     }
   },
 
-  // TODO: Detail delivery address by id
   async detail(request: Request, response: Response): Promise<any> {
-    const { ID } = request.params;
+    const { id } = request.params;
+
     try {
       const deliveryAddressRepository = getRepository(DeliveryAddress);
 
       const deliveryAddress = await deliveryAddressRepository.findOne({
-        where: { id: ID },
+        where: { id },
       });
-      return response.json(deliveryAddress);
+
+      return response.status(200).json(deliveryAddress);
     } catch (error) {
       return response.status(400).json({ message: 'Ocorreu um erro.' });
     }
   },
 
-  // TODO: Update delivery address by id
   async update(request: Request, response: Response): Promise<any> {
     const { id } = request.params;
+
     const {
       fullname,
       zip_code,
@@ -108,22 +108,28 @@ const deliveryAddressController = {
         })
         .where('id = :id', { id })
         .execute();
-      return response.json(result);
+
+      return response.status(200).json(result);
     } catch (error) {
       return response.status(400).json({ message: 'Ocorreu um erro.' });
     }
   },
 
-  // TODO: Create new delivery address
   async delete(request: Request, response: Response): Promise<any> {
     const { id } = request.params;
-    const result = await getRepository(DeliveryAddress)
-      .createQueryBuilder()
-      .delete()
-      .from(DeliveryAddress)
-      .where('id = :id', { id })
-      .execute();
-    return response.status(200).json(result);
+
+    try {
+      const result = await getRepository(DeliveryAddress)
+        .createQueryBuilder()
+        .delete()
+        .from(DeliveryAddress)
+        .where('id = :id', { id })
+        .execute();
+
+      return response.status(200).json(result);
+    } catch (error) {
+      return response.status(400).json({ message: 'Ocorreu um erro.' });
+    }
   },
 };
 
--- src/routes/delivery-address.routes.ts
+++ src/routes/delivery-address.routes.ts
@@ -4,19 +4,14 @@ import deliveryAddressController from '../controller/DeliveryAddressController';
 
 const deliveryAddressRouter = Router();
 
-// TODO: List all delivery address, just adresses
 deliveryAddressRouter.get('/:customerID', deliveryAddressController.read);
 
-// TODO: Create new delivery address
 deliveryAddressRouter.post('/:customerID', deliveryAddressController.create);
 
-// TODO: Detail delivery address by id
-deliveryAddressRouter.get('/:id/:customerID', deliveryAddressController.detail);
+deliveryAddressRouter.get('/:id/', deliveryAddressController.detail);
 
-// TODO: Update delivery address by id
 deliveryAddressRouter.put('/:id/', deliveryAddressController.update);
 
-// TODO: Delete delivery address by id
 deliveryAddressRouter.delete('/:id/', deliveryAddressController.delete);
 
 export default deliveryAddressRouter;
